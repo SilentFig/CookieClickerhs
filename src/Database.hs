6129bbb068b8ea0ca98bb3a55940d5ce27db2782
SilentFig <lowfrequencysilence@gmail.com>
2026-06-16 14:41:49 -0300

    Debug DATABASE_URL

--- src/Database.hs
+++ src/Database.hs
@@ -13,11 +13,31 @@ import Database.Persist.Postgresql
 import Database.Persist (Filter, (==.))
 import Models (migrateAll, Upgrade(..), EntityField(UpgradeName))
 import Control.Monad (when)
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+import qualified Data.ByteString.Char8 as BS
+
+-- | Obtém a URL de conexão ao PostgreSQL.
+getDatabaseUrl :: IO String
+getDatabaseUrl = do
+  mUrl <- lookupEnv "DATABASE_URL"
+
+  putStrLn $
+    "DATABASE_URL encontrada? " ++
+    case mUrl of
+      Nothing -> "NAO"
+      Just _  -> "SIM"
+
+  pure $ fromMaybe
+           "host=localhost dbname=cookieclicker user=postgres password=postgres port=5432"
+           mUrl
 
 -- | Cria um pool de conexões com o PostgreSQL
--- (Na prática, esses dados de acesso devem vir do ambiente/configuração)
 makePool :: IO ConnectionPool
-makePool = runStdoutLoggingT $ createPostgresqlPool "host=localhost dbname=cookieclicker user=postgres password=postgres port=5432" 10
+makePool = do
+  connStr <- getDatabaseUrl
+  runStdoutLoggingT $
+    createPostgresqlPool (BS.pack connStr) 10
 
 -- | Aplica automaticamente as migrations geradas pelo Template Haskell no nosso DB.
 doMigrations :: ConnectionPool -> IO ()
@@ -31,16 +51,17 @@ seedUpgrades :: ConnectionPool -> IO ()
 seedUpgrades pool = runDb pool $ do
   let upserts =
         [ ("Cursor Auto",     Upgrade "Cursor Auto"     "Clica automaticamente."            15          0.5    0.0  1)
-        , ("Vov\xf3",         Upgrade "Vov\xf3"         "Assa cookies deliciosos."          100         4      0.0  2)
-        , ("Fazenda",         Upgrade "Fazenda"         "Sementes m\xe1gicas."               1100        32     0.0  4)
-        , ("Mina",            Upgrade "Mina"            "Cristais puros de a\xe7ucar."       12000       260    0.0  8)
+        , ("Vovó",            Upgrade "Vovó"            "Assa cookies deliciosos."          100         4      0.0  2)
+        , ("Fazenda",         Upgrade "Fazenda"         "Sementes mágicas."                 1100        32     0.0  4)
+        , ("Mina",            Upgrade "Mina"            "Cristais puros de açúcar."         12000       260    0.0  8)
         , ("Receita Secreta", Upgrade "Receita Secreta" "Aumenta seu CPS total em 50%."     50000       0      0.50 16)
-        , ("F\xe1brica",      Upgrade "F\xe1brica"      "Produ\xe7\xe3o em massa de cookies." 130000      1400   0.0  32)
-        , ("Laborat\xf3rio",  Upgrade "Laborat\xf3rio"  "Pesquisa avan\xe7ada de cookies."   1400000     7800   0.0  64)
-        , ("Portal",          Upgrade "Portal"          "Importa cookies de outra dimens\xe3o." 20000000 44000  0.0  128)
-        , ("M\xe1quina do Tempo", Upgrade "M\xe1quina do Tempo" "Busca cookies do futuro."   330000000   260000 0.0  256)
+        , ("Fábrica",         Upgrade "Fábrica"         "Produção em massa de cookies."     130000      1400   0.0  32)
+        , ("Laboratório",     Upgrade "Laboratório"     "Pesquisa avançada de cookies."     1400000     7800   0.0  64)
+        , ("Portal",          Upgrade "Portal"          "Importa cookies de outra dimensão." 20000000   44000  0.0  128)
+        , ("Máquina do Tempo", Upgrade "Máquina do Tempo" "Busca cookies do futuro."       330000000   260000 0.0  256)
         , ("Antimaterial",    Upgrade "Antimaterial"    "Multiplica seu CPS em 2x."         1000000000  0      1.0  512)
         ]
+
   mapM_ (\(name, upg) -> do
     c <- count [UpgradeName ==. name]
     when (c == 0) $ insert_ upg
